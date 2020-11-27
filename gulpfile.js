3d08945bbf7373bc693489949b362046e4adbb05
janitzek <janitzek@neubaukompass.de>
2020-11-27 05:09:14 +0100

    Gulpfile changes

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const DESTINATION = './public';
 
 /**
  * Types to extract
+ * Current supported video types
  */
 const TYPES = 'mp4,MP4,m4p,M4P,m4v,M4V,avchd,AVCHD,flv,FLV,swf,SWF,wmv,WMV,qt,QT,avi,AVI,mp3,MP3,mov,MOV,3gp,3GP,ogg,OGG,webm,WEBM,mpg,MPG,mp2,MP2,mpeg,MPEG,mpe,MPE,mpv,MPV';
 
